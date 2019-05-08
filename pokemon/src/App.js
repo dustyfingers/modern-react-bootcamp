e59323c3efd47bcd3b1303898368d0c30890f7a0
Louie Williford <dustyfingers01257@gmail.com>
2019-05-08 02:22:52 -0600

    styled and finished pokemon project

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
