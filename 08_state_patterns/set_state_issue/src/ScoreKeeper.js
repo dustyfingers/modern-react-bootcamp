71c657535ef275e7080391a9b6d353d6b25c2010
Louie Williford <dustyfingers01257@gmail.com>
2019-05-09 21:51:58 -0600

    learning how to set state in react apps and the differences between state and props

---
+++ 08_state_patterns/set_state_issue/src/ScoreKeeper.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+
+class ScoreKeeper extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { score: 0 };
+		this.singleKill = this.singleKill.bind(this);
+		this.tripleKill = this.tripleKill.bind(this);
+	}
+
+	incrementScore(currentState) {
+		return { score: currentState.score + 1 };
+	}
+	
+	singleKill() {
+		this.setState(this.incrementScore);
+	}
+
+	tripleKill() {
+		this.setState(this.incrementScore);
+		this.setState(this.incrementScore);
+		this.setState(this.incrementScore);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Score is: {this.state.score}</h1>
+				<button onClick={this.singleKill}>Single Kill</button>
+				<button onClick={this.tripleKill}>Triple Kill</button>
+			</div>
+		)
+	}
+}
+
+export default ScoreKeeper;
