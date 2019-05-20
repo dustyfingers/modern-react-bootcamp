73a4fecd4527a1903169e8876bff0a7cc47ef9b2
dustyfingers <dustyfingers01257@gmail.com>
2019-05-19 23:24:45 -0600

    finished simple box builder project

--- 14_form_exercises/box-maker/src/Box.js
+++ 14_form_exercises/box-maker/src/Box.js
@@ -10,7 +10,7 @@ class Box extends Component {
           backgroundColor: this.props.color
           }}
         />
-      <button>X</button>
+      <button onClick={this.props.removeBox}>X</button>
       </div>
     );
   }
--- 14_form_exercises/box-maker/src/BoxList.js
+++ 14_form_exercises/box-maker/src/BoxList.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import NewBoxForm from './NewBoxForm';
 
 
 class BoxList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      boxes: [{ width: 10, height: 10, color: 'orange' }]
+      boxes: []
     }
+    this.create = this.create.bind(this);
+  }
+  remove(id) {
+    this.setState({
+      boxes: this.state.boxes.filter(box => box.id !== id)
+    });
+  }
+  create(newBox) {
+    this.setState({
+      boxes: [...this.state.boxes, newBox]
+    });
   }
   render() {
     const boxes = this.state.boxes.map(box => (
       <Box
+        key={box.id}
+        id={box.id}
         width={box.width}
         height={box.height}
         color={box.color}
+        removeBox={() => this.remove(box.id)}
       />
     ))
     return (
       <div>
         <h1>COLOR BOX MAKER</h1>
+        <NewBoxForm createBox={this.create}/>
         {boxes}
       </div>
     );
