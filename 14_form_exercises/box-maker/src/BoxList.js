05bca9e91f25204e34d1cf61480707aae6d18246
dustyfingers <dustyfingers01257@gmail.com>
2019-05-19 22:42:59 -0600

    working on react forms

---
+++ 14_form_exercises/box-maker/src/BoxList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Box from './Box';
+
+
+class BoxList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boxes: [{ width: 10, height: 10, color: 'orange' }]
+    }
+  }
+  render() {
+    const boxes = this.state.boxes.map(box => (
+      <Box
+        width={box.width}
+        height={box.height}
+        color={box.color}
+      />
+    ))
+    return (
+      <div>
+        <h1>COLOR BOX MAKER</h1>
+        {boxes}
+      </div>
+    );
+  }
+}
+
+export default BoxList;
