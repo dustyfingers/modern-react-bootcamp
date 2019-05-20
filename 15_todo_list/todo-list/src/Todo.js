ba483aeae2fdd91ac04e41ecfeb2b8fe47719507
dustyfingers <dustyfingers01257@gmail.com>
2019-05-20 15:34:59 -0600

    built funcionality to add todo items to the todo list

---
+++ 15_todo_list/todo-list/src/Todo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
+  render() {
+    return (
+      <div>
+        <button>Edit</button>
+        <button>X</button>
+        <li>{this.props.task}</li>
+      </div>
+    );
+  }
+}
+
+export default Todo;
--- 15_todo_list/todo-list/src/TodoList.js
+++ 15_todo_list/todo-list/src/TodoList.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
 
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: [] };
+    this.state = { todos: [{ task: 'walk the fish' }, { task: 'feed the bear' }] };
+    this.create = this.create.bind(this);
+  }
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
   }
   render() {
+    const todos = this.state.todos.map(todo => {
+      return <Todo task={todo.task} />;
+    })
     return (
       <div>
         <h1>Todo List</h1>
         <ul>
-          <li>Todo 1</li>
+          {todos}
         </ul>
+        <NewTodoForm createTodo={this.create}/>
       </div>
     );
   }
