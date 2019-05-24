ca534038fe026e7ba1f61586b62f09159e70da03
dustyfingers <dustyfingers01257@gmail.com>
2019-05-23 21:03:27 -0600

    added functionality of pulling 10 random jokes from dad jokes api

--- 19_dad_jokes/dad-jokes-app/src/JokeList.js
+++ 19_dad_jokes/dad-jokes-app/src/JokeList.js
@@ -3,15 +3,33 @@ import axios from 'axios';
 
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  }
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
   async componentDidMount() {
     // load jokes
-    let res = await axios.get('https://icanhazdadjoke.com/');
-    console.log(res);
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get('https://icanhazdadjoke.com/', {
+        headers: { Accept: 'application/json' }
+      });
+      jokes.push(res.data.joke);
+    }
+    this.setState({ jokes: jokes });
   }
   render() {
     return (
-      <div>
+      <div className='JokeList'>
         <h1>Dad Jokes</h1>
+        <div className='JokeList-jokes'>
+          {this.state.jokes.map(j => {
+            return <div>{j}</div>;
+          })}
+        </div>
       </div>
     );
   }
