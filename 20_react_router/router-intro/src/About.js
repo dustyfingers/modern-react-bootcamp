d5c948f0d6f4f56af034fbc9b57fc1330d5342e0
dustyfingers <dustyfingers01257@gmail.com>
2019-05-25 17:46:06 -0600

    learned about client side 'routing'

---
+++ 20_react_router/router-intro/src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is about the website...</p>
+      </div>
+    )
+  }
+}
+
+export default About;
---
+++ 20_react_router/router-intro/src/Dog.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Dog extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dog</h1>
+        <img src=''/>
+      </div>
+    )
+  }
+}
+
+export default Dog;
