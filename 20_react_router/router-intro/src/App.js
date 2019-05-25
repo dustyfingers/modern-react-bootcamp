fdbcaeea10a4c3571860add907433211d92e4b6e
dustyfingers <dustyfingers01257@gmail.com>
2019-05-25 17:48:53 -0600

    basic react router boilerplate

--- 20_react_router/router-intro/src/App.js
+++ 20_react_router/router-intro/src/App.js
@@ -5,27 +5,9 @@ import Contact from './Contact';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { page: 'about'};
-  }
-  changePage(newPage) {
-    this.setState({ page: newPage });
-  }
-  renderPage() {
-    if (this.state.page === 'about') return <About />
-    if (this.state.page === 'contact') return <Contact />
-    if (this.state.page === 'dog') return <Dog />
-  }
   render() {
     return (
       <div className="App">
-        <nav>
-          <a onClick={() => this.changePage('about')}>About</a>
-          <a onClick={() => this.changePage('contact')}>Contact</a>
-          <a onClick={() => this.changePage('dog')}>Dog</a>
-        </nav>
-        {this.renderPage()}
       </div>
     );
   }
