8408a7a95b784da1167a346354cbefeda3bca879
dustyfingers <dustyfingers01257@gmail.com>
2019-05-25 13:30:06 -0600

    added vote order to jokes

--- 19_dad_jokes/dad-jokes-app/src/JokeList.js
+++ 19_dad_jokes/dad-jokes-app/src/JokeList.js
@@ -17,24 +17,32 @@ class JokeList extends Component {
     };
     this.handleVote = this.handleVote.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.seenJokes = new Set(this.state.jokes.map(j => j.text));
   }
   componentDidMount() {
     if (this.state.jokes.length === 0) this.getJokes();
   }
   async getJokes() {
-    let jokes = [];
-    while (jokes.length < this.props.numJokesToGet) {
-      let res = await axios.get('https://icanhazdadjoke.com/', {
-        headers: { Accept: 'application/json' }
-      });
-      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
+    try {
+      let jokes = [];
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get('https://icanhazdadjoke.com/', {
+          headers: { Accept: 'application/json' }
+        });
+        let newJoke = res.data.joke;
+        if (!this.seenJokes.has(newJoke)) jokes.push({ id: uuid(), text: newJoke, votes: 0 });
+      }
+      this.setState(st => ({
+        jokes: [...st.jokes, ...jokes],
+        loading: false
+      }),
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+      )
     }
-    this.setState(st => ({
-      jokes: [...st.jokes, ...jokes],
-      loading: false
-    }),
-    () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
-  );
+    catch(e) {
+      alert(e);
+      this.setState({ loading: false });
+    }
   }
   handleVote(id, delta) {
     this.setState(
@@ -57,6 +65,7 @@ class JokeList extends Component {
         </div>
       )
     }
+    let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
     return (
       <div className='JokeList'>
         <div className='JokeList-sidebar'>
@@ -64,9 +73,8 @@ class JokeList extends Component {
             <img src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/198/face-with-tears-of-joy_1f602.png' alt='emoji'/>
             <button className='JokeList-getmore' onClick={this.handleClick}>New Jokes</button>
         </div>
-
         <div className='JokeList-jokes'>
-          {this.state.jokes.map(j => (
+          {jokes.map(j => (
             <Joke
               upvote={() => {this.handleVote(j.id, 1)}}
               downvote={() => {this.handleVote(j.id, -1)}}
