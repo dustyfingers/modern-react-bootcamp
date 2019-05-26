d64705cbfe1b5002c2caf80c9b1c6eee38be17b7
dustyfingers <dustyfingers01257@gmail.com>
2019-05-25 19:15:07 -0600

    learned about react router!

--- 20_react_router/router-intro/src/App.js
+++ 20_react_router/router-intro/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import Dog from './Dog';
 import About from './About';
 import Contact from './Contact';
 import './App.css';
 
+
+const Hater = () => <h1>I hate dogs!!</h1>;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className='App-nav'>
+          <NavLink exact activeClassName='active-link' to='/'>About</NavLink>
+          <NavLink exact activeClassName='active-link' to='/contact'>Contact</NavLink>
+          <NavLink exact activeClassName='active-link' to='/dog'>Dog</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/dog' render={() => <Dog name='PEANUT' />} />
+          <Route exact path='/dog/hater' component={Hater} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
       </div>
     );
   }
--- 20_react_router/router-intro/src/Dog.js
+++ 20_react_router/router-intro/src/Dog.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 
 class Dog extends Component {
+  componentDidMount() {
+    console.log('DOG DID MOUNT');
+  }
+  componentWillUnmount() {
+    console.log('DOG WILL UNMOUNT');
+  }
   render() {
+    console.log('DOG DID RENDER');
     return (
       <div>
         <h1>Dog</h1>
-        <img src=''/>
+        <p>THIS DOG IS NAMED {this.props.name}!!!!</p>
       </div>
     )
   }
