e7bce197609d7be48745129447ab1b875543a617
dustyfingers <dustyfingers01257@gmail.com>
2019-05-27 13:38:57 -0600

    added doglist component

--- 23_router_exercises/dog-finder/src/App.js
+++ 23_router_exercises/dog-finder/src/App.js
@@ -1,49 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import DogList from './DogList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+class App extends Component {
+  static defaultProps ={
+    dogs: [
+      {
+        name: 'Peanut',
+        age: 2,
+        facts: [
+          'Loves the park!',
+          'Loves his mama!',
+          'Loves to play!'
+        ]
+      },
+      {
+        name: 'Keeva',
+        age: 5,
+        facts: [
+          'Loves to snuggle!',
+          'Loves her papa!',
+          'Loves to eat food!'
+        ]
+      },
+      {
+        name: 'Lissa',
+        age: 4,
+        facts: [
+          'Loves to snuggle!',
+          'Loves her papa!',
+          'Loves to play with Peanut!'
+        ]
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <Route path='/dogs' render={ () => <DogList dogs={this.props.dogs} /> } />
       </div>
-    </nav>
-      </div>
-      <h1 className='display-1'>DOG APP!</h1>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ 23_router_exercises/dog-finder/src/DogList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './DogList.css';
+
+
+class DogList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div className='DogList'>
+                <h1 className='display-1 text-center'>DOG LIST</h1>
+                <div className='container'>
+                    <div className='row'>
+                        {this.props.dogs.map(dog => (
+                            <div className='Dog col-md-6 col-lg-4 text-center' key={dog.name}>
+                                <h3>{dog.name}</h3>
+                                <img src='https://source.unsplash.com/800x800/?dog' alt={dog.name} />
+                            </div>
+                        ))}
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DogList;
