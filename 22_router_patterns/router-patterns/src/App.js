0f4a08cfb8dc58f0c9e17b01918d8834f7e864eb
dustyfingers <dustyfingers01257@gmail.com>
2019-05-27 00:14:50 -0600

    learned about url params with react router

--- 22_router_patterns/router-patterns/src/App.js
+++ 22_router_patterns/router-patterns/src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Food from './Food';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Food name='Kale' />
-      <Food name='Carrot' />
-      <Food name='Ice Cream' />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App"> 
+        <Route 
+          exact 
+          path='/food/:name' 
+          render={ routeProps => <Food {...routeProps} /> } 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
