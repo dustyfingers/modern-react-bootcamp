8bdc443b6c84eb6ca8e9278bdb7167e885d2c1b5
dustyfingers <dustyfingers01257@gmail.com>
2019-06-04 15:27:24 -0600

    mini palette ugly version! to learn css in js

--- color-app/src/PaletteList.js
+++ color-app/src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
     constructor(props) {
@@ -10,6 +11,7 @@ class PaletteList extends Component {
         const {palettes} = this.props;
         return (
             <div>
+                <MiniPalette />
                 <h1>React Colors</h1>
                 {palettes.map( palette => (
                     <p><Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link></p>
