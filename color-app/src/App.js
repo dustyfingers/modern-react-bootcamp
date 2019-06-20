1bf3b9da50c6027c6a8b5611d02f80a68a7df95d
dustyfingers <dustyfingers01257@gmail.com>
2019-06-20 13:10:57 -0600

    created new palette form component

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -3,13 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import SingleColorPalette from './SingleColorPalette';
 import PaletteList from './PaletteList';
+import NewPaletteForm from './NewPaletteForm';
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import "./App.css";
 
 class App extends Component {
 	findPalette(id) {
-		return seedColors.find(function(palette) {
+		return seedColors.find(function (palette) {
 			return palette.id === id;
 		});
 	}
@@ -18,27 +19,32 @@ class App extends Component {
 			<Switch>
 				<Route
 					exact
+					path="/palette/new"
+					render={() => <NewPaletteForm />}
+				/>
+				<Route
+					exact
 					path="/palette/:paletteId/:colorId"
-					render={ routeProps => (
+					render={routeProps => (
 						<SingleColorPalette
 							colorId={routeProps.match.params.colorId}
 							palette={generatePalette(
 								this.findPalette(routeProps.match.params.paletteId)
-							)} 
+							)}
 						/>
 					)}
 				/>
-				<Route 
-					exact 
-					path="/" 
-					render={ routeProps => (
+				<Route
+					exact
+					path="/"
+					render={routeProps => (
 						<PaletteList palettes={seedColors} {...routeProps} />
-					)} 
+					)}
 				/>
 				<Route
 					exact
 					path="/palette/:id"
-					render={ routeProps => (
+					render={routeProps => (
 						<Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
 					)}
 				/>
