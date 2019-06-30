0ce7038a127a0b5657f61b0972bda3d32cde2add
dustyfingers <dustyfingers01257@gmail.com>
2019-06-30 07:59:54 -0600

    learned about hooks in react 16.8 by building counter components

---
+++ hooks-demo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CounterClass from './CounterClass';
+import CounterHooks from './CounterHooks';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterClass />
+      <CounterHooks />
+    </div>
+  );
+}
+
+export default App;
