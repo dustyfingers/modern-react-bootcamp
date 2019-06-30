1033444140c0485f7a304b213f6f7f4179af57ee
dustyfingers <dustyfingers01257@gmail.com>
2019-06-30 16:21:21 -0600

    refactored styles out to separate files to shorten components

---
+++ color-app/src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,33 @@
+const styles = {
+    root: {
+        height: '25%',
+        width: '20%',
+        margin: '0 auto',
+        display: 'inline-block',
+        position: 'relative',
+        cursor: 'pointer',
+        marginBottom: '-3.5px',
+        "&:hover svg": {
+            color: 'white',
+            transform: 'scale(1.25)'
+        }
+    },
+    boxContent: {
+        position: 'absolute',
+        color: 'rgba(0, 0, 0, 0.5)',
+        width: '100%',
+        left: '0px',
+        bottom: '0px',
+        padding: '10px',
+        letterPpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    deleteIcon: {
+        transition: 'all 0.3s ease-in-out'
+    }
+};
+
+export default styles;
